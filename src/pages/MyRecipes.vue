09f64d15c03d337439a21a6f93f744cea886a35d
itayrazum <83130169+itayrazum@users.noreply.github.com>
2024-09-19 13:49:41 +0300

    Update MyRecipes.vue

--- src/pages/MyRecipes.vue
+++ src/pages/MyRecipes.vue
@@ -1,43 +1,71 @@
 <template>
-    <div class="container">
-      <h1 class="title">My Recipes</h1>
-      <b-row>
-        <b-col v-for="recipe in myRecipes" :key="recipe.id" cols="12" md="6" lg="4">
-          <RecipePreview :recipe="recipe" />
-        </b-col>
-      </b-row>
-    </div>
-  </template>
-  
-  <script>
-  import RecipePreview from "../components/RecipePreview.vue";
-  
-  export default {
-    components: {
-      RecipePreview
-    },
-    data() {
-      return {
-        myRecipes: []
-      };
-    },
-    mounted() {
-      this.loadMyRecipes();
-    },
-    methods: {
-      loadMyRecipes() {
-        let recipes = JSON.parse(localStorage.getItem('recipes')) || [];
-        this.myRecipes = recipes;
+  <div class="container">
+    <h1 class="title">My Recipes</h1>
+    <b-row v-if="loading">
+      <b-col class="text-center">
+        <b-spinner label="Loading..."></b-spinner>
+      </b-col>
+    </b-row>
+    <b-row v-else-if="myRecipes.length === 0">
+      <b-col class="text-center">
+        <p>You haven't created any recipes yet.</p>
+      </b-col>
+    </b-row>
+    <b-row v-else>
+      <b-col v-for="recipe in myRecipes" :key="recipe.recipe_id" cols="12" md="6" lg="4">
+        <RecipePreview :recipe="recipe" />
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import RecipePreview from "../components/RecipePreview.vue";
+import axios from 'axios';
+
+export default {
+  components: {
+    RecipePreview
+  },
+  data() {
+    return {
+      myRecipes: [],
+      loading: true
+    };
+  },
+  mounted() {
+    this.loadMyRecipes();
+  },
+  methods: {
+    async loadMyRecipes() {
+      try {
+        const username = localStorage.getItem('username');
+        if (!username) {
+          throw new Error("User not logged in");
+        }
+
+        const response = await axios.get(`http://localhost:80/recipes/custom/${username}`);
+
+        if (response.data.success) {
+          this.myRecipes = response.data.recipes;
+        } else {
+          throw new Error(response.data.message || "Failed to load recipes");
+        }
+      } catch (error) {
+        console.error("Error loading recipes:", error);
+        this.$root.toast("Error", error.message || "Failed to load recipes", "danger");
+      } finally {
+        this.loading = false;
       }
     }
-  };
-  </script>
-  
-  <style scoped>
-  .title {
-    margin-top: 20px;
-    margin-bottom: 20px;
-    text-align: center;
   }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.title {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+</style>
